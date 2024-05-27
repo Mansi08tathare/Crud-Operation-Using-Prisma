b1f22c59b132689ae0c75722d829c25eae58ae8d
Mansi08tathare <tatharemansi76@gmail.com>
2024-05-27 09:55:16 +0530

    commit

--- src/books/books.service.ts
+++ src/books/books.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import {PrismaService} from '../prisma.service'
 import { Prisma } from '@prisma/client';
 
@@ -44,6 +44,8 @@ export class BooksService {
         id
       }
     });
-    return resp
+    return {
+      status:HttpStatus.OK
+    }
   }
 }
